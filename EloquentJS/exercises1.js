4575e3abf0da42bd707ab5b4123a7a5c264f1cec
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2025-01-09 22:54:57 +0200

    FizzBuzz

---
+++ EloquentJS/exercises1.js
@@ -0,0 +1,51 @@
+// Write a loop that makes seven calls to console.log to output the following triangle:
+//
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+
+function printTriangle () {
+    let printable ='';
+    for (let i = 0; i < 7; i++) {
+        for (let j = 0; j < i; j++) {
+            printable = printable + '#';
+        }
+        console.log(printable)
+        printable = '';
+    }
+}
+
+//Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions.
+// For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3),
+// print "Buzz" instead.
+//
+// When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible by both 3 and 5
+// (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
+
+function fizzBuzz() {
+    count = 0;
+    printable = '';
+
+    while (count <= 100) {
+        if (count % 3 === 0) {
+            printable = printable + "Fizz";
+        } if (count % 5 === 0) {
+            printable = printable + "Buzz";
+        }
+        if (printable !== "") {
+            console.log(printable);
+        } else {
+            console.log(count);
+        }
+        count++;
+        printable = '';
+
+    }
+}
+
+printTriangle();
+fizzBuzz();
