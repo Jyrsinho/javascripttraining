cf80aa5ab29c79a0525745750c6afe9c761910ad
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2025-01-11 20:34:42 +0200

    isEven handles all but negative numbers

---
+++ EloquentJS/isEven.js
@@ -0,0 +1,16 @@
+
+
+/**
+ *  Zero is even. One is odd. For any other number N, its evenness is the same as N - 2.
+ *  Function returns whether a given number is even using this rule.
+ */
+function isEven(number) {
+    //basecase
+    if (number ===0) return true;
+    if (number ===1) return false;
+    //recursive step
+
+     return isEven(number - 2);
+}
+
+module.exports = isEven;
---
+++ EloquentJS/isEven.test.js
@@ -0,0 +1,25 @@
+const isEven = require('./isEven')
+
+test('isEven should return true for 0',  ()=> {
+    expect(isEven(0)).toBe(true);
+})
+
+test('isEven should return false for 1',  ()=> {
+    expect(isEven(1)).toBe(false);
+})
+
+test('isEven should return true for 2',  ()=> {
+    expect(isEven(2)).toBe(true);
+})
+
+test('isEven should return false for 3',  ()=> {
+    expect(isEven(3)).toBe(false);
+})
+
+test('isEven should return true for 400',  ()=> {
+    expect(isEven(400)).toBe(true);
+})
+
+test('isEven should return false for -1',  ()=> {
+    expect(isEven(-1)).toBe(false);
+})
