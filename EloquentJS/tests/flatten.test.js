7a46a6e40dabf9f328608507254eae07b44d48c8
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2025-01-15 20:59:34 +0200

    flatten handles also empty nested arrays and arrays with only one nested array

--- EloquentJS/tests/flatten.test.js
+++ EloquentJS/tests/flatten.test.js
@@ -2,6 +2,7 @@ const flatten = require("../flatten");
 
 
 
+
 test("flatten should return an empty array when given an empty array", () => {
         let testArray = [];
         expect(flatten(testArray)).toStrictEqual([]);
@@ -16,4 +17,14 @@ test ('flatten should flatten two arrays into one', () => {
 test ('flatten should flatten three arrays into one', () => {
     let testArray = [[1,2,3], [4], [5,6,7]] ;
     expect(flatten(testArray)).toStrictEqual([1,2,3,4,5,6,7]);
+})
+
+test ('flatten should return same array when given onedimensional array', () => {
+    let testArray = [[1,2,3]];
+    expect(flatten(testArray)).toStrictEqual([1,2,3]);
+})
+
+test ('flatten should flatten when given three arrays with one empty', () => {
+    let testArray = [[1,2,3], [], [4,5,6]];
+    expect(flatten(testArray)).toStrictEqual([1,2,3,4,5,6]);
 })
