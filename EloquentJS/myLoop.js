fc6d681ebd2c9bb513594870f0fd8a0ac54e7424
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2025-01-16 21:30:33 +0200

    myLoop can print values from 3 to zero

--- EloquentJS/myLoop.js
+++ EloquentJS/myLoop.js
@@ -1,2 +1,13 @@
 
-function myLoop(value, test, update, body) {}
+function myLoop(value, test, update, body) {
+
+    while (test(value)) {
+        body(value);
+        value = update(value);
+    }
+
+}
+
+
+
+myLoop(3, n => n > 0, n => n - 1, console.log);
