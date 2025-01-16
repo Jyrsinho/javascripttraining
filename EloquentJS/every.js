319fbd97f60609f095f0687d98efb6510bc550d0
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2025-01-16 22:23:49 +0200

    tried to find implementation for every using built in some function and de morgans laws - not working

--- EloquentJS/every.js
+++ EloquentJS/every.js
@@ -1,3 +1,9 @@
+/**
+ * tests if an array's every element passes the given test
+ * @param array to be tested
+ * @param test function that does the testing
+ * @returns {boolean} true if all the elements pass the test, false if not
+ */
 function every (array, test) {
     for (let i = 0; i < array.length; i++) {
 
@@ -8,10 +14,15 @@ function every (array, test) {
     return true;
 }
 
-
+/**
+ * Same purpose as function every. this one uses built in some method
+ * @param array
+ * @param test
+ */
 function everysome(array, test) {
-
-
+    if (!array.some(test)) {
+        return false;
+    }
 }
 
 console.log(every([1, 3, 5], n => n < 10));
@@ -19,4 +30,13 @@ console.log(every([1, 3, 5], n => n < 10));
 console.log(every([2, 4, 16], n => n < 10));
 // → false
 console.log(every([], n => n < 10));
+// → true
+
+console.log();
+
+console.log(everysome([1, 3, 5], n => n < 10));
+// → true
+console.log(everysome([2, 4, 16], n => n < 10));
+// → false
+console.log(everysome([], n => n < 10));
 // → true
