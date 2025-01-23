55245616b249d05403d02b6cf0e7467bd2926fd0
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2025-01-23 21:22:22 +0200

    following along Eloquent JS chapter 7 project

--- EloquentJS/meadowfield/app.js
+++ EloquentJS/meadowfield/app.js
@@ -26,6 +26,29 @@ function buildGraph(edges) {
     return graph;
 }
 
+
+function runRobot(state, robot, memory) {
+    for (let turn = 0;; turn++) {
+        if (state.parcels.length == 0) {
+            console.log(`Done in ${turn} turns`);
+            break;
+        }
+        let action = robot(state, memory);
+        state = state.move(action.direction);
+        memory = action.memory;
+        console.log(`Moved to ${action.direction}`);
+    }
+}
+
+function randomPick(array) {
+    let choice = Math.floor(Math.random() * array.length);
+    return array[choice];
+}
+
+function randomRobot(state) {
+    return {direction: randomPick(roadGraph[state.place])};
+}
+
 const roadGraph = buildGraph(roads);
 console.log(roadGraph);
 
