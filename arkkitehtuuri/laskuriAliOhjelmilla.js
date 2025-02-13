8cf2fac8d0d82a184e0445daa472604816ee1a82
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2025-02-13 18:29:48 +0200

    laskuri aliOhjelmilla 6

--- arkkitehtuuri/laskuriAliOhjelmilla.js
+++ arkkitehtuuri/laskuriAliOhjelmilla.js
@@ -1,28 +1,36 @@
 const prompt = require('prompt-sync')();
 
 function paaOhjelma() {
-    let vastaukset = kysySyote();
-    console.log(vastaukset);
+    let validiVastaus = false;
 
-    let virhe = validoi(vastaukset);
-
-    if (virhe === "") {
-        tulos = valitseLaskuToimitus(vastaukset);
-        console.log(tulos);
+    while (validiVastaus === false) {
+        let luku1 = kysySyote("Anna luku 1 >");
+        validiVastaus = tarkistaSyote(luku1);
     }
-    else
-    console.log(virhe);
+    let validiVastaus2 = false;
+    while (validiVastaus2 === false) {
+        let luku2 = kysySyote("Anna luku 2 >");
+        validiVastaus2 = tarkistaSyote(luku2);
+    }
+    let laskutoimitus = kysySyote("Anna laskutoimitus >");
+
+    let tulos = suoritaLaskutoimitus(laskutoimitus, luku1, luku2);
+}
+
+
+function tarkistaSyote(syote) {
+    if (Number.isNaN(syote)) {
+        return false;
+    }
+    return true;
 
 }
 
-function kysySyote() {
-    const vastaukset = {};
 
-    vastaukset.luku1 = prompt("Anna luku1 > ");
-    vastaukset.luku2 = prompt("Anna luku2 > ");
-    vastaukset.laskutoimitus = prompt("Anna laskuToimitus > ");
+function kysySyote(viesti) {
+    vastaus = prompt(viesti);
 
-    return vastaukset;
+    return vastaus;
 }
 
 
