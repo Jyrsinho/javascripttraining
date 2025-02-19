21cfa06348694599f26bd6953d1a79a5271b10f1
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2025-02-19 14:31:38 +0200

    fetch-api harjoituksia

--- EloquentJS/forms.mjs
+++ EloquentJS/forms.mjs
@@ -1,3 +1,6 @@
+// These formats are identified by the standardized media types text/plain, text/html, and application/json.
+
+
 let url = "https://eloquentjavascript.net/author ";
 
 
@@ -6,12 +9,37 @@ const response = await fetch(url,
 {
     method: "GET",
         headers: {
-    Accept: "application/json",
-},
+    Accept: "application/json"
+}
 }
 ).then(response => response.json()).then(data => {
-    console.log(data)
-    })
-;
+    console.log(data.name);
+    console.log(data.email);
+    console.log(data.website);
+    });
+
+// luetaan plaintext data
+const responsePlainText = await (fetch(url,
+    {
+        method: "GET",
+        headers: {
+            Accept: "text/plain"
+        }
+    }
+    ).then(response => response.text().then(data =>  {
+        console.log(data);
+})));
+
+// luetaan plain html
+const responseHTML = await (fetch(url,
+    {
+        method: "GET",
+        headers: {
+            Accept: "text/html"
+        }
+    })).then(response => response.text().then(data =>  {
+        console.log(data);
+}))
+
 
 
